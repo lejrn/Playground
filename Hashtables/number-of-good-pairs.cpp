5aa421e97ee56a6e8ba1800c16f9185171d9c86c
lejrn <87588082+lejrn@users.noreply.github.com>
2022-09-17 15:38:11 +0300

    Solved

--- Hashtables/number-of-good-pairs.cpp
+++ Hashtables/number-of-good-pairs.cpp
@@ -4,15 +4,25 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
-int gp(int arr[]){
-    int ans = 0;
-
-
-    return ans;
-}
+class Solution {
+public:
+    int numIdenticalPairs(std::vector<int>& nums) {
+        int ans = 0;
+        std::unordered_map<int,int> hash;
+        for(int i=0; i<nums.size();++i){
+            int occurrence = hash[nums[i]];
+            hash[nums[i]]++;
+            ans += occurrence;
+        }
+        return ans;
+    }
+};
 
 int main(){
-
+    Solution ans;
+    std::vector<int> nums{1,2,3,1,1,3};
+    std::cout << ans.numIdenticalPairs(nums) << std::endl;
     return 0;
 }
