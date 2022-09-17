55023f1641fe77117f6c9a35462d1ffa96a66524
lejrn <87588082+lejrn@users.noreply.github.com>
2022-09-17 15:00:10 +0300

    Adding gp function

--- Hashtables/number-of-good-pairs.cpp
+++ Hashtables/number-of-good-pairs.cpp
@@ -2,3 +2,17 @@
 // Created by USER on 17/09/2022.
 //
 
+#include <iostream>
+#include <unordered_map>
+
+int gp(int arr[]){
+    int ans = 0;
+
+
+    return ans;
+}
+
+int main(){
+
+    return 0;
+}
