ab9aa2f218de07e1c106853c4bfca2533fab2ab5
lejrn <87588082+lejrn@users.noreply.github.com>
2022-09-17 17:04:13 +0300

    Solved

---
+++ Hashtables/jewels-and-stones.cpp
@@ -0,0 +1,36 @@
+//
+// Created by USER on 17/09/2022.
+//
+
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int numJewelsInStones1(std::string jewels, std::string stones) {
+        std::unordered_map<char,int> hash;
+        int ans=0;
+        for(auto j : jewels) hash[j]++;
+        for(auto s : stones) ans+=hash[s];
+        return ans;
+    }
+
+    int numJewelsInStones2(std::string jewels, std::string stones) {
+        std::unordered_set<char> set(jewels.begin(), jewels.end());
+        int ans=0;
+        for(auto s : stones) ans+=set.contains(s);
+        return ans;
+    }
+};
+
+/*        */
+
+int main(){
+    std::string j="aA",s="aAAbbbb";
+    Solution ans;
+    std::cout << ans.numJewelsInStones1(j,s) << std::endl;
+    return 0;
+}
